cb040dc56292723caf0ab1aaffd622298cd09dfb
Mansi <mansins168@gmail.com>
2022-11-03 16:36:05 +0530

    login

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express=require("express");
 const app=express();
-const port=3000;
+const port=4000;
 const mongoose=require("mongoose");
 const {MONGOURI}=require('./keys');
-require('./models/user');
+
+
+
 mongoose.model("User");
 mongoose.connect(MONGOURI);
+
 mongoose.connection.on('connected',()=>{
     console.log("Database functional");
 })
@@ -13,6 +16,12 @@ mongoose.connection.on('error',(err)=>{
     console.log(err);
 })
 
+require('./models/user');
+require('./models/post')
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+
 app.listen(port,function(){
-    console.log("Port 3000");
+    console.log("Port 4000");
 });
--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://kirti2810:dQSEZUXgAG4tNQHv@atlascluster.hqcdd.mongodb.net/?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://kirti2810:dQSEZUXgAG4tNQHv@atlascluster.hqcdd.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"jdeygfbsmvfgwejd43edb78weur"
 }
